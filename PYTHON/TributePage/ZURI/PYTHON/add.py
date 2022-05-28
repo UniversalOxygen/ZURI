e472fab43f81f128d28acd3448f16322d32cfe2d
OLAGOKE TUNDE OLUJIDE <90968664+UnivesalOxygen@users.noreply.github.com>
2022-05-29 00:43:50 +0800

    Delete PYTHON/TributePage/ZURI/PYTHON directory

--- PYTHON/TributePage/ZURI/PYTHON/add.py
+++
@@ -1,7 +0,0 @@
-A = int(input("input first value: "))
-B = int(input("input second value: "))
-
-sum = (A + B)
-
-
-print("Total sum of the values: ", sum)
--- PYTHON/TributePage/ZURI/PYTHON/count.py
+++
@@ -1,8 +0,0 @@
-count = 0
-sentence = input("Enter your sentence: ")
-
-for space in sentence:
-    if space == " ":
-        count += 1
-number_of_words = count + 1
-print("Number of words : ", number_of_words)
